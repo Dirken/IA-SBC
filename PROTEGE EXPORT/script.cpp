40a2e026ef3cf0c41d6602c79a1e5465533ae992
PC-DAVID\dracu <david.marin.info@gmail.com>
2016-11-30 00:31:30 +0100

    Makefile updated and script updated. all.clp updated: New instances and corrections.

--- PROTEGE EXPORT/script.cpp
+++ PROTEGE EXPORT/script.cpp
@@ -8,24 +8,18 @@ int main(){
     ifstream loadFile;
     loadFile.open("classes.pont");
     if (loadFile.is_open()) {
-	for(int i=0; i<=3; ++i) getline(loadFile, linea);
+        for(int i=0; i<=3; ++i) getline(loadFile, linea);
         while (getline(loadFile, linea)) outputFile << linea + "\n";
         cout << "Archivo classes.pont leido correctamente." << endl;
-    } else {
-        cout << "Archivo classes.pont no encontrado o no ha podido abrirse." << endl;
-        exit(1);
-    } 
+    } else { cout << "Archivo classes.pont no encontrado o no ha podido abrirse." << endl; exit(1); } 
     loadFile.close();
     outputFile << "\n(definstances Instances\n";
     loadFile.open("instances.pins");
     if (loadFile.is_open()) {
-	for(int i=0; i<=3; ++i) getline(loadFile, linea);
+        for(int i=0; i<=3; ++i) getline(loadFile, linea);
         while (getline(loadFile, linea)) outputFile << "\t" + linea + "\n";
-        cout << "Archivo instances.pins leido correctamente.";
-    } else {
-        cout << "Archivo instances.pins no encontrado o no ha podido abrirse.";
-        exit(1);
-    }
+        cout << "Archivo instances.pins leido correctamente." << endl;
+    } else { cout << "Archivo instances.pins no encontrado o no ha podido abrirse."; exit(1); }
     loadFile.close();
     outputFile << ")" << endl;
     outputFile.close();
