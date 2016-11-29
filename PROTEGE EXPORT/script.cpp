924f77d3fa5cfc9ed5c954d197f910fad0e0a933
DuveD <david.marin.info@gmail.com>
2016-11-29 17:09:52 +0100

    Script updated. Added makefile

--- PROTEGE EXPORT/script.cpp
+++ PROTEGE EXPORT/script.cpp
@@ -1,41 +1,30 @@
 #include <fstream>
 #include <iostream>
-
 using namespace std;
 
-int main(){ // g++ -std=c++11  -o script script.cpp
+int main(){
     ofstream outputFile("all.clp"); 
-
     string linea;
-
-    ifstream loadFile1;
-    loadFile1.open("classes.pont");
-    if (loadFile1.is_open()) {    //Si existe el archivo lo leemos
-        while (getline(loadFile1, linea)) { //Mientras existan palabras
-            outputFile << linea + "\n";
-        }
+    ifstream loadFile;
+    loadFile.open("classes.pont");
+    if (loadFile.is_open()) {
+        while (getline(loadFile, linea)) outputFile << linea + "\n";
         cout << "Archivo classes.pont leido correctamente." << endl;
     } else {
         cout << "Archivo classes.pont no encontrado o no ha podido abrirse." << endl;
         exit(1);
     } 
-    loadFile1.close();
-
+    loadFile.close();
     outputFile << "\n(definstances Instances\n";
-
-    ifstream loadFile2;
-    loadFile2.open("instances.pins");
-    if (loadFile2.is_open()) {    //Si existe el archivo lo leemos
-        while (getline(loadFile2, linea)) { //Mientras existan palabras
-            outputFile << "\t" + linea + "\n";
-        }
+    loadFile.open("instances.pins");
+    if (loadFile.is_open()) {
+        while (getline(loadFile, linea)) outputFile << "\t" + linea + "\n";
         cout << "Archivo instances.pins leido correctamente.";
     } else {
         cout << "Archivo instances.pins no encontrado o no ha podido abrirse.";
         exit(1);
     }
-    loadFile2.close();
-
+    loadFile.close();
     outputFile << ")" << endl;
     outputFile.close();
-}
+} 
